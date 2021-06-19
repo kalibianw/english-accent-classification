1112ddffe30cfd05213ca2b7104b7a22f50629cb
kalibianw <kalibianw@gmail.com>
2021-06-20 02:13:20 +0900

    Implementing training data preprocessing function.

---
+++ preprocessing.py
@@ -0,0 +1,10 @@
+from utils import DataModule
+import numpy as np
+
+DATA_DIR_PATH = "D:/AI/data/english accent classification/"
+
+# for training data
+dm = DataModule(data_dir_path=DATA_DIR_PATH, training_dir_name="train", test_dir_name="test")
+features, norm_features, labels = dm.training_data_preprocessing(10, 80)
+
+print(np.shape(features), np.shape(norm_features), np.shape(labels))
---
+++ utils.py
@@ -0,0 +1,49 @@
+from pydub.utils import mediainfo
+import numpy as np
+import librosa
+import os
+
+
+class DataModule:
+    def __init__(self, data_dir_path: str, training_dir_name: str, test_dir_name: str):
+        """
+        :param data_dir_path: The path must end with a '/'
+        :param training_dir_name: ex) "train"
+        :param test_dir_name: ex) "test"
+        """
+        self.DATA_DIR_PATH = data_dir_path
+        self.TRAIN_DATA_DIR_PATH = data_dir_path + training_dir_name + "/"
+        self.TEST_DATA_DIR_PATH = data_dir_path + test_dir_name + "/"
+
+    def training_data_preprocessing(self, cut_sec: int, n_mfcc: int):
+        label_list = os.listdir(self.TRAIN_DATA_DIR_PATH)
+
+        features = list()
+        norm_features = list()
+        labels = list()
+        for label_index, label in enumerate(label_list):
+            flist = os.listdir(self.TRAIN_DATA_DIR_PATH + label)
+            per = len(flist) / 100
+            for file_index, fname in enumerate(flist):
+                audio_path = self.TRAIN_DATA_DIR_PATH + label + "/" + fname
+                audio, sr = librosa.load(path=audio_path, sr=int(mediainfo(audio_path)["sample_rate"]))
+
+                if np.shape(audio)[0] < sr * cut_sec:
+                    expand_arr = np.zeros(shape=((sr * cut_sec) - np.shape(audio)[0]), dtype="float32")
+                    audio = np.append(audio, expand_arr)
+                elif np.shape(audio)[0] > sr * cut_sec:
+                    cutted_arr = np.split(ary=audio, indices_or_sections=(sr * cut_sec,))
+                    audio = cutted_arr[0]
+
+                audio_mfcc = librosa.feature.mfcc(audio, sr=sr, n_mfcc=n_mfcc)
+                features.append(audio_mfcc)
+                audio_mfcc_norm = librosa.util.normalize(audio_mfcc)
+                norm_features.append(audio_mfcc_norm)
+                labels.append(label_index)
+
+                if (file_index % int(per)) == 0:
+                    print(f"현재 {file_index}개 완료, {(file_index / per)}%")
+
+        print(np.shape(features), np.shape(norm_features), np.shape(labels))
+
+        return features, norm_features, labels
